143a859b23a8e063dc7dbfd69629d8fee31f2538
Chao Pan <panchao1985@hotmail.com>
2023-09-30 14:39:55 +0800

    update macro

--- RODS/macro.h
+++ RODS/macro.h
@@ -1,16 +1,19 @@
+#define CONCAT(X,Y) X##Y
+#define TO_STR(X) #X
+
 #define JSON_TO_ELEMENTS(ELE_TYPE, PARA)\
-auto j_array_##ELE_TYPE = model.at(#ELE_TYPE##"Vec");\
+auto j_array_##ELE_TYPE = model.at(TO_STR(ELE_TYPE##Vec));\
 ELE_TYPE ELE_TYPE##Obj;\
-for (auto i = 0; i<model.at(#ELE_TYPE##"Count"); i++)\
+for (auto i = 0; i<model.at(TO_STR(ELE_TYPE##Count)); i++)\
 {\
     j_array_##ELE_TYPE[i].get_to(ELE_TYPE##Obj);\
     add##ELE_TYPE(ELE_TYPE##Obj.id, ELE_TYPE##Obj.IdDofI, ELE_TYPE##Obj.IdDofJ, ELE_TYPE##Obj.PARA);\
 }
 
 #define JSON_TO_ELEMENTS_2(ELE_TYPE, PARA1, PARA2)\
-auto j_array_##ELE_TYPE = model.at(#ELE_TYPE##"Vec");\
+auto j_array_##ELE_TYPE = model.at(TO_STR(ELE_TYPE##Vec));\
 ELE_TYPE ELE_TYPE##Obj;\
-for (auto i = 0; i<model.at(#ELE_TYPE##"Count"); i++)\
+for (auto i = 0; i<model.at(TO_STR(ELE_TYPE##Count)); i++)\
 {\
     j_array_##ELE_TYPE[i].get_to(ELE_TYPE##Obj);\
     add##ELE_TYPE(ELE_TYPE##Obj.id, ELE_TYPE##Obj.IdDofI, ELE_TYPE##Obj.IdDofJ,\
@@ -18,9 +21,9 @@ for (auto i = 0; i<model.at(#ELE_TYPE##"Count"); i++)\
 }
 
 #define JSON_TO_ELEMENTS_3(ELE_TYPE, PARA1, PARA2, PARA3)\
-auto j_array_##ELE_TYPE = model.at(#ELE_TYPE##"Vec");\
+auto j_array_##ELE_TYPE = model.at(TO_STR(ELE_TYPE##Vec));\
 ELE_TYPE ELE_TYPE##Obj;\
-for (auto i = 0; i<model.at(#ELE_TYPE##"Count"); i++)\
+for (auto i = 0; i<model.at(TO_STR(ELE_TYPE##Count)); i++)\
 {\
     j_array_##ELE_TYPE[i].get_to(ELE_TYPE##Obj);\
     add##ELE_TYPE(ELE_TYPE##Obj.id, ELE_TYPE##Obj.IdDofI, ELE_TYPE##Obj.IdDofJ,\
@@ -28,8 +31,8 @@ for (auto i = 0; i<model.at(#ELE_TYPE##"Count"); i++)\
 }
 
 #define ELEMENTS_TO_JSON(ELE_TYPE)\
-model[#ELE_TYPE##"Count"] = ELE_TYPE##s.size();\
+model[TO_STR(ELE_TYPE##Count)] = ELE_TYPE##s.size();\
 vector<ELE_TYPE> ELE_TYPE##Vec;\
 for (auto it = ELE_TYPE##s.begin(); it != ELE_TYPE##s.end(); it++)\
     ELE_TYPE##Vec.push_back(*(it->second));\
-model[#ELE_TYPE##"Vec"] = ELE_TYPE##Vec;
+model[TO_STR(ELE_TYPE##Vec)] = ELE_TYPE##Vec;
